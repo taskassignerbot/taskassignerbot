75c1873f9f20717c6840d5d6670fa02dffe9d197
cishero6 <98536586+cishero6@users.noreply.github.com>
2024-05-08 14:19:57 +0300

    Implement AssemblyAi

--- text_extractor.py
+++ text_extractor.py
@@ -3,6 +3,7 @@ import speech_recognition as sr
 import soundfile as sf
 import tempfile
 from transformers import pipeline
+import assemblyai as aai
 
 def convert_to_wav(voice):
     voice.download('file.ogg')
@@ -19,7 +20,7 @@ async def process_voice_file_and_get_text(voice_file):
         await voice_file.download_to_drive(custom_path=ogg_file_path)
         data, samplerate = sf.read(ogg_file_path)
         sf.write(wav_file_path, data, samplerate)
-        return recognize_text_from_voice_temp(wav_file_path)
+        return recognize_assembly(wav_file_path)
 
 def recognize_text_from_voice(file_path):
     recognizer = sr.Recognizer()
@@ -28,8 +29,16 @@ def recognize_text_from_voice(file_path):
     text = recognizer.recognize_sphinx(audio)
     return text
 
-# глеб, вот эту можешь функцию вообще убрать, только в process_voice_file_and_get_text поменяй на нужную
-def recognize_text_from_voice_temp(file_path):
-    pipe = pipeline("automatic-speech-recognition", model="facebook/wav2vec2-base-100h")
-    ans = pipe(file_path)
-    return ans
+
+
+def recognize_assembly(file_path): #РАБОЧИЙ ВАРИАНТ
+    aai.settings.api_key = get_assemblyai_api_key()
+    config = aai.TranscriptionConfig(
+    word_boost=["Gleb", "Danil Petrov"],
+    boost_param="high",
+    language_code="ru",
+    )
+    transcriber = aai.Transcriber(config=config)
+    transcript = transcriber.transcribe(file_path)
+    print(transcript.words)
+    return transcript.text
